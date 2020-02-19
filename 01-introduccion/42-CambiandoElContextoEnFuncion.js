ab47bef66f7d03c48b64112d4f41129cdb06f798
mariomt <mariotinoco85@gmail.com>
2020-02-19 10:34:25 -0700

    se agrego ejemplo de cuando cambia el contexto de una funcion

---
+++ 01-introduccion/42-CambiandoElContextoEnFuncion.js
@@ -0,0 +1,24 @@
+const persona1 = {
+    nombre: 'Mario',
+    apellido: 'Murillo'
+}
+
+function saludar(saludo = 'Hola') {
+    console.log(`${saludo}, mi nombre es ${this.nombre}`);
+}
+
+
+// lo que hace el bind es regresar una funcion diferente con el contexto cambiado
+//const saludarAPersona1 = saludar.bind(persona1); 
+
+// setTimeout(saludar.bind(persona1,'Hola che'),1000);
+
+// si hacemos lo siguiente no se ejecutará nada ya que solo nos regresa la referencia a la funcion
+// mas no se ejecuta.
+// saludar.bind(persona1,'Hola che');
+
+
+// si queremos ejecutar una función cambiando su contexto es necesario usar la función call
+saludar.call(persona1,'Hola Che');
+saludar.apply(persona1, ['Hola che']);
+
